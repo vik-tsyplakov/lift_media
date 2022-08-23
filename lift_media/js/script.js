e856599d91f51cf86f3e846cdd291539bd70ac42
github_vik-tsyplakov <vik.tsyplakov@gmail.com>
2022-08-23 21:11:27 +0300

    block-slider-nft start slider

--- lift_media/js/script.js
+++ lift_media/js/script.js
@@ -38,3 +38,13 @@ function closeOnClick() {
   hamb.classList.remove("active");
   body.classList.remove("noscroll");
 }
+
+// Swiper slider
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
